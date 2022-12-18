625228b60f38c0246a20fead9e59fdf70b18d20a
HermandoThiago <hermandodev@gmail.com>
2022-12-18 19:27:08 -0300

    style: card styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <>
-      <p className="text-lg font-bold text-center">Meu nome é Hermando!</p>
-    </>
+    <section className="h-screen w-screen flex items-center justify-center">
+      <div className="border border-slate-300 px-10 py-12 rounded-md shadow-2xl">
+          <p className="text-white text-2xl">
+            Hermando Thiago
+          </p>
+      </div>
+    </section>
   )
 }
