78dc4973b691970a1326d08fa778c1c7bad38fca
HermandoThiago <hermandodev@gmail.com>
2022-12-29 17:09:20 -0300

    feat: responsive mobile

--- src/components/IconsContainer/IconsContainer.tsx
+++ src/components/IconsContainer/IconsContainer.tsx
@@ -1,12 +1,12 @@
-export function IconsContainer(){
-    return (
-        <div className="flex justify-center items-center flex-row text-white text-4xl py-5 space-x-4 w-full">
-            <i className="devicon-typescript-plain colored"></i>
-            <i className="devicon-nextjs-original"></i>
-            <i className="devicon-nestjs-plain colored"></i>
-            <i className="devicon-docker-plain colored"></i>
-            <i className="devicon-postgresql-plain colored"></i>
-            <i className="devicon-python-plain colored"></i>
-          </div>
-    )
+export function IconsContainer() {
+  return (
+    <div className="flex justify-center items-center flex-row text-white text-4xl py-5 space-x-4 w-full">
+      <i className="devicon-typescript-plain colored cursor-pointer hover:scale-110"></i>
+      <i className="devicon-nextjs-original cursor-pointer hover:scale-110"></i>
+      <i className="devicon-nestjs-plain colored cursor-pointer hover:scale-110"></i>
+      <i className="devicon-docker-plain colored cursor-pointer hover:scale-110"></i>
+      <i className="devicon-postgresql-plain colored cursor-pointer hover:scale-110"></i>
+      <i className="devicon-python-plain colored cursor-pointer hover:scale-110"></i>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,42 @@
-import { Button, IconsContainer, Photo } from "~/components"
-import { BsGithub, BsLinkedin, BsMedium } from "react-icons/bs"
-import { MdEmail } from "react-icons/md"
+import { Button, IconsContainer, Photo } from "~/components";
+import { BsGithub, BsLinkedin, BsMedium } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
 
 export default function Home() {
   return (
     <section className="h-screen w-screen flex items-center justify-center">
-      <div className="border-cyan-300 border border-opacity-10 py-12 rounded-md shadow-2xl max-w-screen-sm text-center">
-          <Photo />
-          <p className="text-white text-3xl">
-            Hermando Thiago
-          </p>
-          <p className="text-white py-1 text-lg">
-            Fullstack Typescript
-          </p>
-          <IconsContainer />
-          <div>
-            <Button 
-              link="https://github.com/HermandoThiago"
-              icon={<BsGithub size={20}/>}  
-            >
-              Github
-            </Button>
-            <Button 
-              link="https://www.linkedin.com/in/hermando-thiago/"
-              icon={<BsLinkedin size={20} />}
-            >
-              Linkedin
-            </Button>
-            <Button 
-              link="mailto:hermandodev@gmail.com"
-              icon={<MdEmail size={20} />}
-            >
-              Email
-            </Button>
-            <Button 
-              link="https://medium.com/@hermandodev"
-              icon={<BsMedium size={20} />}
-            >
-              Medium
-            </Button>
-          </div>
+      <div className="border-cyan-300 border border-opacity-10 py-12 rounded-md shadow-2xl max-w-md text-center">
+        <Photo />
+        <p className="text-white text-3xl">Hermando Thiago</p>
+        <p className="text-white py-1 text-lg">Fullstack Typescript</p>
+        <IconsContainer />
+        <div>
+          <Button
+            link="https://github.com/HermandoThiago"
+            icon={<BsGithub size={20} />}
+          >
+            Github
+          </Button>
+          <Button
+            link="https://www.linkedin.com/in/hermando-thiago/"
+            icon={<BsLinkedin size={20} />}
+          >
+            Linkedin
+          </Button>
+          <Button
+            link="mailto:hermandodev@gmail.com"
+            icon={<MdEmail size={20} />}
+          >
+            Email
+          </Button>
+          <Button
+            link="https://medium.com/@hermandodev"
+            icon={<BsMedium size={20} />}
+          >
+            Medium
+          </Button>
+        </div>
       </div>
     </section>
-  )
+  );
 }
