759a085201b397b2dd258f9c877d4abe424b5dfd
HermandoThiago <hermandodev@gmail.com>
2022-12-29 17:23:24 -0300

    refactor: sizes in mobile

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,21 +2,26 @@ import Link from "next/link";
 import { ReactNode } from "react";
 
 interface IPropsButton {
-    children: ReactNode
-    link: string
-    icon?: ReactNode
+  children: ReactNode;
+  link: string;
+  icon?: ReactNode;
 }
 
-export function Button({ children, link, icon }: IPropsButton){
-    return (
-        <button 
-            className="
-                bg-cyan-300 font-bold text-cyan-900 w-80 rounded-md py-3 hover:scale-110 duration-150
+export function Button({ children, link, icon }: IPropsButton) {
+  return (
+    <button
+      className="
+                bg-cyan-300 font-bold text-cyan-900 w-full rounded-md py-3 hover:scale-110 duration-150
                 hover:origin-bottom hover:rotate-3 my-2 shadow-2xl 
-                ">
-            <Link className="flex justify-center items-center" href={link} target="_blank">
-               {icon} <span className="mx-1">{children}</span>
-            </Link>
-        </button>
-    )
+                "
+    >
+      <Link
+        className="flex justify-center items-center"
+        href={link}
+        target="_blank"
+      >
+        {icon} <span className="mx-1">{children}</span>
+      </Link>
+    </button>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { MdEmail } from "react-icons/md";
 export default function Home() {
   return (
     <section className="h-screen w-screen flex items-center justify-center">
-      <div className="border-cyan-300 border border-opacity-10 py-12 rounded-md shadow-2xl max-w-md text-center">
+      <div className="border-cyan-300 border border-opacity-10 py-8 px-10 rounded-md shadow-2xl max-w-sm text-center">
         <Photo />
         <p className="text-white text-3xl">Hermando Thiago</p>
         <p className="text-white py-1 text-lg">Fullstack Typescript</p>
